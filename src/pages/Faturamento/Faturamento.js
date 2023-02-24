ddbd004613b72742b69c65eeabadcc8a6342f72e
Eliel Silva <79846053+eliel1986@users.noreply.github.com>
2023-02-24 12:43:07 -0300

    upgrade

--- src/pages/Faturamento/Faturamento.js
+++ src/pages/Faturamento/Faturamento.js
@@ -1,7 +1,48 @@
 export function Faturamento() {
     return (
-      <div>
-        <h1>FATURAMENTO</h1>
-      </div>
+      
+      
+       
+        <form>
+            <p>Densidade origem</p>
+            <input type="number" id="dens_orig" placeholder="0.000">
+
+            <p>Densidade destino</p>
+            <input type="number" id="dens_dest" placeholder="0.000">
+
+            <p>Peso origem</p>
+            <input type="number" id="peso_orig" placeholder="Kg">
+
+            <p>Peso destino</p>
+            <input type="number" id="peso_dest" placeholder="Kg">
+            <p></p>
+            <button id="btn_calc" type="button">Calcular</button>
+
+            <p>Peso destino / Densidade destino</p>
+            <input type="text" id="pDest_dDest">
+
+            <p>Peso destino / Densidade origem</p>
+            <input type="text" id="pDest_dOrig">
+
+            <p>Diferença densidades</p>
+            <input type="text" id="dif_Dens">
+
+            <p>Peso destino - Peso origem</p>
+            <input type="text" id="pDest_pOrig">
+
+            <p>Diferença PESOS / Dens origem
+            </p>
+            <input type="text" id="dif_Peso">
+
+
+
+            <p>Resultado:</p>
+            <input type="text" id="result" >
+
+            <button id="clean" type="reset">Limpar</button>
+
+            <a href="../index.html"><button type="button" >Voltar</button></a>
+        </form>
+    
     );
   }
