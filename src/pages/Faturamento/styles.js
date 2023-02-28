4ab49d081228d8af32529096f2b2dc4c8f4da121
Eliel Silva <79846053+eliel1986@users.noreply.github.com>
2023-02-28 12:45:41 -0300

    1

--- src/pages/Faturamento/styles.js
+++ src/pages/Faturamento/styles.js
@@ -2,78 +2,6 @@ import styled from 'styled-components';
 
 export const Global = styled.body`
 
-/* body {
-    background-color: grey;
-    text-align: center;
-}
-
-@media screen and (min-width: 0px) and (max-width: 270px) {
-    .box {
-        width: 80vw;
-        margin: auto;
-        font-size: 11px;
-    }
-
-    button {
-        width: 40vw;
-    }
-
-    input {
-        width: 40vw;
-    }
-}
-
-@media screen and (min-width: 271px) and (max-width: 400px) {
-    .box {
-        width: 80vw;
-        margin: auto;
-        font-size: 15px;
-    }
-
-    button {
-        width: 40vw;
-    }
-
-    input {
-        font-size: 16px;
-        width: 40vw;
-        height: 20px;
-    }
-}
-
-@media screen and (min-width: 401px) and (max-width: 600px) {
-    .box {
-        width: 60vw;
-        margin: auto;
-        font-size: 17px;
-    }
-
-    button {
-        width: 30vw;
-    }
-
-    input {
-        width: 30vw;
-    }
-}
-
-@media screen and (min-width: 601px) {
-    .box {
-        width: 250px;
-        margin: auto;
-        font-size: 17px;
-    }
-
-    button {
-        width: 130px;
-    }
-
-    input {
-        width: 130px;
-    }
-}
-*/
-
 .box-peso {
     background-color: rgba(0, 0, 0, 0.6);
     padding: 5px;
@@ -96,7 +24,7 @@ input {
 
 button {
     background-color: slateblue;
-    width: 100px;
+    width: 500px;
     margin-top: 8px;
     margin-bottom: 8px;
     padding: 5px;
